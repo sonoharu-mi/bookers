e9039b9e84d039240ffc9504f4e0c4969f0ef925
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-07-20 05:25:34 +0000

    Bookers showまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,27 @@
 class BooksController < ApplicationController
   def new
+    
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
