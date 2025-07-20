e3af1a49e2afc06dee2839ac4319e7d7402a48bc
sonoharu-mi <215863048+sonoharu-mi@users.noreply.github.com>
2025-07-20 13:05:36 +0000

    Bookers updateまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -18,7 +19,13 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @list = Book.find(params[:id])
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
